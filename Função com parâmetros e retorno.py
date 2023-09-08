bd8cca2028bdd59850524a42c006d2f484bf20d6
F0Xdesuu <f0xdesu@outlook.com>
2023-09-07 23:19:16 -0300

    atividade_08

---
+++ Função com parâmetros e retorno.py
@@ -0,0 +1,9 @@
+# Função que calcula o quadrado de um número e o retorna
+def calcular_quadrado(numero):
+    quadrado = numero ** 2
+    return quadrado
+
+# Chamando a função para calcular o quadrado de um número
+numero = 7
+resultado = calcular_quadrado(numero)
+print(f"O quadrado de {numero} é {resultado}.")
---
+++ Função com parâmetros e sem retorno.py
@@ -0,0 +1,7 @@
+# Função que calcula a área de um retângulo
+def calcular_area_retangulo(base, altura):
+    area = base * altura
+    print(f"A área do retângulo com base {base} e altura {altura} é {area}.")
+
+# Chamando a função para calcular a área do retângulo
+calcular_area_retangulo(5, 8)
